03e8337bc3149053cb48c7d72cd791c487c1dc94
Jaesung <jsyoo61@korea.ac.kr>
2022-06-27 22:57:49 +0900

    Merge branch 'dev_temp' into dev

--- hideandseek/node.py
+++ hideandseek/node.py
@@ -1,97 +1,87 @@
 '''
-
-
+might be deprecated. Think this whole validation (testing) can be integrated into eval.py
 '''
+from collections import deque
 import logging
 import os
-import shutil
-from copy import deepcopy as dcopy
 
+import matplotlib.pyplot as plt
+import numpy as np
 import torch
-import torch.optim as optim
-import torch.utils.data as D
+import torch.nn as nn
+import torch.nn.functional as F
 
-import tools # since T is used as a variable in this module, refrain from "import tools as T"
-import tools.torch
+from . import eval as E
+import tools as T
 import tools.modules
-
-from . import validation as V
-from . import utils as U
-from . import plot as P
-
-__all__ = [
-'Node'
-]
 
 # %%
 log = logging.getLogger(__name__)
 
 # %%
-class Node:
-    '''Local Node for training'''
-    def __init__(self, model, dataset=None, cfg_train={}, criterion=None, model_dir=None, node_dir=None, validation=None, earlystopper=None, name='default', verbose=True, amp=False):
+def isbetter(score_best: float, score: float, increase_better: bool):
+    return (not increase_better and score<score_best) or (increase_better and score>score_best)
+
+# %%
+'''
+Validation object specifies a single dataset.
+scorer is tied to each validation object.
+
+
+'''
+
+class Validation:
+    def __init__(self, dataset, d_scorer, batch_size=64):
         '''
-        :param model: torch.nn.Module object
-        :param dataset: torch.utils.data.Dataset object
-            This dataset is used for training.
-            Recommended return format from dataset is: {'x': x, 'y': y}
-            where 'x' is the input, and 'y' is the target values.
-
-
-
-        :param validation: dict of Validation objects
-
-        :param cfg_train: dict-like object which contains:
-            lr
-            batch_size
-            weight_decay (optional)
-            patience (optional)
-
-        :param criterion: torch.nn.Module object, used to compute loss function
-
-        Recommended way of making hs.node.Node object is like the following:
-
-            kwargs = {'model': model, 'dataset': train_dataset, 'validation': None, 'cfg_train': cfg.train,
-                    'criterion': criterion, 'model_dir': path['model'], 'node_dir': path['node'], 'verbose': True, 'amp': True}
-            node = hs.node.Node(**kwargs)
+        :param dataset: torch.utils.data.Dataset object or list of torch.utils.data.Dataset objects
+        :param d_scorer: dict of hideandseek scorer functions which take result(dict) and returns a single score
+            # TODO: May support returning multiple scores for optimization
         '''
-        # Store configurations
-        self.model = model
         self.dataset = dataset
-        self.cfg_train = dcopy(dict(cfg_train))
-        self.criterion = criterion
-        self.model_dir = model_dir
-        self.node_dir = node_dir
-        self.validation = validation if issubclass(type(validation), dict) else V.VDict({'default': validation}) # wrap with VDict if single validation object is given.
-        self.earlystopper = earlystopper
-        self.name = name
-        self.verbose = verbose
-        self.amp = amp
+        self.d_scorer = d_scorer
+        self.batch_size = batch_size
 
-        # Initializations
-        if self.model_dir is None:
-            self.print('model_dir not specified, default to "model"')
-            self.model_dir = 'model'
-        if os.path.exists(self.model_dir): log.warning(f'path: {self.model_dir} exists. Be careful')
-        os.makedirs(self.model_dir, exist_ok=True)
+        self.history = {score_type: T.modules.ValueTracker() for score_type in self.d_scorer.keys()}
 
-        if self.node_dir is None:
-            self.print('node_dir not specified, default to "node"')
-            self.node_dir = 'node'
-        if os.path.exists(self.node_dir): log.warning(f'path: {self.node_dir} exists. Be careful')
-        os.makedirs(self.node_dir, exist_ok=True)
-
-        if self.validation is None and self.earlystopper is not None: log.warning('validation is None but earlystopper is given. earlystopper will be ignored')
-        if self.earlystopper is not None:
-            assert earlystopper.target_validation in self.validation.keys(), 'earlystopper.target_valiation not provided in validation'
-            assert earlystopper
-
-        self.train_meter = tools.modules.AverageMeter()
-        self.loss_tracker = tools.modules.ValueTracker()
-        self.set_misc()
         self.reset()
 
-        self._targets_type = self.infer_targets_type()
+    def step(self, node):
+        amp = node.amp
+
+        ld_results = []
+        # If dataset is a list
+        if type(self.dataset) == list:
+            for dataset_ in self.dataset:
+                d_results = E.test_node(node, dataset_, batch_size=self.batch_size, amp=amp)
+                ld_results.append(d_results)
+            d_results_merged = T.merge_dict(ld_results)
+            results = {key: np.concatenate(results, axis=0) for key, results in d_results_merged}
+        # Single dataset
+        else:
+            results = E.test_node(node, self.dataset, batch_size=self.batch_size, amp=amp)
+
+        # Compute scores using hideandseek eval functions which receives results (dictionary)
+        score = {score_type: scorer(results) for score_type, scorer in self.d_scorer.items()}
+        for score_type, valuetracker in self.history.items():
+            valuetracker.step(node.iter, score[score_type])
+
+        return score
+
+    def plot(self, ax=None, square=False):
+        '''
+        plot validation history
+        '''
+        if ax is None:
+            if square:
+                nrowcol = int(np.ceil(np.sqrt(len(self.d_scorer))))
+                fig, ax = plt.subplots(nrows=nrowcol, ncols=nrowcol)
+            else:
+                fig, ax = plt.subplots(ncols=len(self.d_scorer))
+            ax = ax.flatten()
+        for (score_type, valuetracker), ax_ in zip(self.history.items(), ax):
+            valuetracker.plot(ax=ax_)
+            ax_.set_title(score_type)
+        return ax
 
     def reset(self):
         self.iter = 0
@@ -319,113 +309,160 @@ class Node:
         '''function to call every other epoch. May be used in subclass nodes'''
         pass
 
-    def infer_targets_type(self):
-        if self.dataset is not None and hasattr(self.dataset, 'targets_type'):
-            return self.dataset.targets_type
-        elif self.validation is not None and 'val' in self.validation:
-            if type(self.validation['val'].dataset)==list:
-                return self.validation['val'].dataset[0].targets_type
-            else:
-                return self.validation['val'].dataset.targets_type
+class EarlyStopper():
+    def __repr__(self):
+        return f'<EarlyStopper>\npatience: {self.patience}\nincrease_better: {self.increase_better}'
+
+    def __init__(self, increase_better, patience, primary_score=None, target_validation='default', discard_best=True):
+        self.increase_better = increase_better
+        self.patience = patience
+        self.primary_score = primary_score
+        self.target_validation = target_validation
+        self.discard_best = discard_best
+
+        self.history = T.modules.ValueTracker() # For now, only track single score
+        self.reset()
+
+    def step(self, node, score_summary, path):
+        '''
+        score_summary: 2-level nested dictionary of scores.
+            1st-level keys are validation object names,
+            2nd-level keys are score names
+        '''
+        score_target_validation = score_summary[self.target_validation]
+        if self.primary_score is None:
+            assert len(score_target_validation)==1, f"primary_score must be specified when there's more than 1 scorer functions, received: {len(score_target_validation)}"
+            score = list(score_target_validation.values())[0]
+            # assert type(score) is not dict, 'when primary_score is not given, the given score must be a scalar'
         else:
-            return None
+            score = score_target_validation[self.primary_score]
+        self.history.step(node.iter, score)
 
-    def post_train(self, val_dataset=None):
-        # Deafult to self.validation['val'].dataset
-        if val_dataset is None:
-            assert 'val' in self.validation, f'if no val_dataset is given, then "val" must exist in self.validation: {self.validation.keys()}'
-            val_dataset = self.validation['val'].dataset
+        # Save best model
+        if isbetter(score_best=self.best_score, score=score, increase_better=self.increase_better):
+            log.info(f'[EarlyStopping]New best score: {self.best_score} -> {score}')
+            log.info(f'[EarlyStopping]Saved model: {path}')
+            torch.save(node.model.state_dict(), path)
+            if self.discard_best and self.best_model!=None:
+                os.remove(self.best_model)
 
-        if self._targets_type == 'binary':
-            self.print(f'[Node: {self.name}][post_train] binary classfication: choose threshold based on validation set')
-            self.threshold = E.binary_threshold(self, val_dataset)
+            self.best_score = score
+            self.best_model=path
+
+            # Clear patience tracking
+            self.history.reset()
+
+        # patience_end == True when best model did not appear for self.patience times
+        patience_end = self.patience == len(self.history)
+        log.info(f'[EarlyStopping][patience: {len(self.history)}/{self.patience}]')
+
+        return patience_end
+
+    def reset(self):
+        # log.info('[EarlyStopper] Resetting...')
+        self.history.reset()
+        self.best_model = None
+        if self.increase_better:
+            self.best_score = -float('inf')
         else:
-            self.print(f'[Node: {self.name}][post_train] no post-train procedure for _targets_type: {self._targets_type}')
+            self.best_score = float('inf')
 
+class EarlyStopping(Validation):
+    def __init__(self, dataset, scorer, primary_score=None, increase_better=False, discard_best=True, patience=None):
+        '''
+        :param dataset: dataloader or list of dataloaders
+        '''
+        super().__init__(dataset, scorer)
+        self.increase_better = increase_better
+        self.discard_best = discard_best
+
+        if type(self.scorer)==dict and primary_score==None:
+            self.primary_score=self.scorer.keys()[0] # default to 1st score function
+        else:
+            self.primary_score=primary_score
+
+        if patience is None:
+            self.patience=float('inf')
+            self.cv_history = deque(maxlen=0)
+        else:
+            self.patience=patience
+            self.cv_history = deque(maxlen=patience)
+
+        self.reset()
+
+    def step(self, node, name):
+        score = super().step(node)
+        if type(self.scorer)==dict:
+            score_ = score[self.primary_score]
+        else:
+            score_ = score
+        self.cv_history.append(score_)
+
+        # Save best model
+        if isbetter(score_best=self.best_score, score=score_, increase_better=self.increase_better):
+        # (not self.increase_better and score_<self.best_score) or (self.increase_better and score_>self.best_score):
+            log.info(f'Saved model: {name}')
+            torch.save(node.model.state_dict(), name)
+            if self.discard_best and self.best_model!=None:
+                os.remove(self.best_model)
+
+            self.best_score = score_
+            self.best_model=name
+
+            # Clear patience tracking
+            self.cv_history.clear()
+
+        # patience_end == True when best model did not appear for self.patience times
+        patience_end = self.patience == len(self.cv_history)
+        log.info(f'[EarlyStopping][patience: {len(self.cv_history)}/{self.cv_history.maxlen}]')
+
+        return score, patience_end
+
+    def reset(self):
+        log.info('[EarlyStopping] Resetting...')
+        self.cv_history.clear()
+        self.best_model = None
+        if self.increase_better:
+            self.best_score = -float('inf')
+        else:
+            self.best_score = float('inf')
+
+# %%
+class VDict(dict):
+    def reset(self):
+        for v in self.values():
+            v.reset()
+
+    def plot(self, axes, savepath='.'):
+        """
+        Plot per score.
+        Each axis corresponds to one scorer.
+        In each axis, multiple lines correspond to multiple validation objects (datasets).
+        """
+        # Plot validation
+        for score_type, ax in zip(d_scorer.keys(), axes.flatten()):
+            P.plot_values([v.history[score_type] for v in self.values()], ax=ax)
+            legend = [score_type+suffix for score_type, suffix in it.product(self.keys(), ['','_smoothed'])]
+            ax.set_title(score_type)
+            ax.legend(legend)
+        fig.savefig(os.path.join(savepath,'validation.png'))
+        return ax
+
+def track_score(score_tracker_dict, score_dict, x=None, label=None):
     '''
-    Save/Load functions
-    state_dict()
-    load_state_dict()
-
-    save()
-    save_best()
-    load()
+    :score_tracker_dict, score_dict: dict
     '''
-    def state_dict(self):
-        state_dict = {
-        # 'cv_tracker': self.cv_tracker,
-        'loss_tracker': self.loss_tracker,
-        } # tools.TDict raises error when called in tools.save_pickle
-        if 'get_f' in self.misc:
-            state_dict['misc.get_f'] = self.misc.get_f
-        if hasattr(self, 'threshold'):
-            state_dict['threshold'] = self.threshold
-        return state_dict
+    assert score_tracker_dict.keys() == score_dict.keys()
+    keys = score_dict.keys()
 
-    def load_state_dict(self, state_dict):
-        state_dict = dcopy(state_dict)
+    '''the keys may not be ordered. just use keys to reference'''
+    # for k in keys:
+    #     T = len(score_tracker_dict[k].x)
+    #     score_tracker[k].step(T+1, score_dict[k])
 
-        if 'misc.get_f' in state_dict.keys():
-            self.print(f'Updating get_f: {state_dict["misc.get_f"]}')
-            self.misc.get_f = state_dict['misc.get_f']
-            if self.dataset is not None:
-                self.print('updating get_f to loader...')
-                self.dataset.get_f = self.misc.get_f
-            del state_dict['misc.get_f']
-
-        if 'threshold' in state_dict.keys():
-            self.print(f'Updating threshold: {state_dict["threshold"]}')
-            self.threshold = state_dict['threshold']
-            del state_dict['threshold']
-
-        self.__dict__.update(state_dict) # update attributes
-
-    def save(self, path=None, best=True):
-        '''
-        Save the following:
-        state_dict() -> path/state_dict.p
-        model.state_dict() -> path/model.pt
-        '''
-        path = self.node_dir if path is None else path
-        self.print(f'[Node: {self.name}]')
-
-        state_dict_path = os.path.join(path, 'node.p')
-        self.print(f'[save] Saving node info to: {state_dict_path}')
-        state_dict = self.state_dict()
-        self.print(f'[save] state_dict: {list(state_dict.keys())}')
-        tools.save_pickle(state_dict, state_dict_path)
-
-        model_path = os.path.join(path, 'model.pt')
-        if best:
-            if self.earlystopper is None:
-                self.print(f'[save][best: {best}] earlystopper not defined. Saving current model -> [{model_path}]')
-                torch.save(self.model.state_dict(), model_path)
-            else:
-                if self.earlystopper.best_model != None:
-                    self.print(f'[save][best: {best}] Saving [{self.earlystopper.best_model}] -> [{model_path}]')
-                    shutil.copy(self.earlystopper.best_model, model_path)
-                else:
-                    self.print(f'[save][best: {best}] no best_model in self.earlystopper, Saving current model -> [{model_path}]')
-                    torch.save(self.model.state_dict(), model_path)
+    for score_tracker, score in zip(score_tracker_dict.values(), score_dict.values()):
+        if x ==None:
+            T = len(score_tracker.x)
+            score_tracker.step(T+1, score, label=label)
         else:
-            self.print(f'[save][best: {best}] Saving model to: {model_path}')
-            torch.save(self.model.state_dict(), model_path)
-
-    def load(self, path=None):
-        '''
-        load state_dict() and model:
-        path/node.p -> state_dict
-        path/model.pt -> model.state_dict
-        '''
-        path = self.node_dir if path is None else path
-        self.print(f'[Node: {self.name}]')
-
-        state_dict_path = os.path.join(path, 'node.p')
-        self.print(f'[load] Loading from path: {state_dict_path}')
-        state_dict = tools.load_pickle(state_dict_path)
-        self.print(f'[load] Updating: {list(state_dict.keys())}')
-        self.load_state_dict(state_dict)
-
-        model_path = os.path.join(path, 'model.pt')
-        self.print(f'[load] Loading model from: {model_path}')
-        self.model.load_state_dict(torch.load(model_path))
+            score_tracker.step(x, score, label=label)
